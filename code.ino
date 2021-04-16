4584c35cd83d93fa1e0a8cb38a60773950f4486d
deanwadley <81776892+deanwadley@users.noreply.github.com>
2021-04-16 15:48:45 -0400

    Add files via upload

--- code.ino
+++ code.ino
@@ -35,6 +35,8 @@ if(digitalRead(2)==HIGH)
 }
 
 void loop() {
+    int allotted_time = 5000; //5 seconds?
+    int start_time, current_time;
 
 
     while(game_running)
@@ -44,30 +46,51 @@ void loop() {
       
       for(int i=0; i<99; i++)  //this means for a max of 99 rounds
       {
-        task = rand(0,2);  //let choose_it=0...flip_it=1...squeeze_it=2
+        task = random(0,3);  //let choose_it=0...flip_it=1...squeeze_it=2
 
         if(task == 0) //if choose_it
         {
           outputsound(0);
           int which_buttons;
-          which_buttons = rand(0,2);   //this determines which buttons to press
+          which_buttons = random(0,3);   //this determines which buttons to press
           
-          currenttime = millis();
+          start_time = millis();
           clock_ticking = 1;
           while(clock_ticking)
           {
+              current_time = millis();
+              if (current_time >= start_time + allotted_time)
+              {
+                game_running = 0;
+              }
 
-
-
+              if(which_buttons == 0)
+              {
+                if(digitalRead(6)==HIGH && digitalRead(7)==HIGH && digitalRead(8)==LOW)
+                break;
+              }
+              if(which_buttons == 1)
+              {
+                if(digitalRead(7)==HIGH && digitalRead(8)==HIGH && digitalRead(6)==LOW)
+                break;
+              }
+              if(which_buttons == 2)
+              {
+                if(digitalRead(6)==HIGH && digitalRead(8)==HIGH && digitalRead(7)==LOW)
+                break;
+              }
+                  
 
             
           }
-          
+          score = score + 1;
           
           
           
           
         }
+
+        allotted_time -= 10; //minus a tenth of a second for the next round
       }
 
      
